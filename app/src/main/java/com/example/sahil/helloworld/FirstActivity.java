96bd6facdf196352fb70bc2802252dcf691bb657
niharika636 <niharikachawla02@gmail.com>
2018-06-26 12:44:30 +0530

    fcommit

--- app/src/main/java/com/example/sahil/helloworld/FirstActivity.java
+++ app/src/main/java/com/example/sahil/helloworld/FirstActivity.java
@@ -2,6 +2,8 @@ package com.example.sahil.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"CLICKED!",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
